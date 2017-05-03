3f9d37e1ebf9bcca6036cfbc85654f46da6da279
Andrew <email@andrewlord.co.uk>
2017-05-03 20:28:40 +0100

    added main component

---
+++ app/components/RecipeBookApp.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var RecipeBookApp = React.createClass({
+    render: function()
+    {
+        return (
+            <div>ReactApp</div>
+        );
+    }
+});
+
+module.exports = RecipeBookApp;
